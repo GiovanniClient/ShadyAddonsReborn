32972896d810854cc403b5ad05551db37818a137
GiovanniClient <giofonta289@proton.me>
2025-01-23 17:56:15 +0100

    updated Rat.java :P

--- src/main/java/cheaters/get/banned/remote/Rat.java
+++ src/main/java/cheaters/get/banned/remote/Rat.java
@@ -1,10 +1,15 @@
 package cheaters.get.banned.remote;
 
 public class Rat {
+        public static String line1 = "         .-\"\"\"-.";
+        public static String line2 = "        /       \\";
+        public static String line3 = "        \\       /";
+        public static String line4 = " .-\"\"\"-.-`.-.-.<  _";
+        public static String line5 = "/      _,-\\ ()()_/:)";
+        public static String line6 = "\\     / ,  `     `|";
+        public static String line7 = " '-..-| \\-.,___,  /";
+        public static String line8 = "       \\ `-.__/  /";
+        public static String line9 = "  jgs   `-.__.-'";
 
-        String a = "                  ..----.._    _              ";
-        String b = "                .' .--.    '-.(O)_            ";
-        String c = "    '-.__.-'''=:|  ,  _)_ |__ . c\'-..        ";
-        String d = "                 ''------'---''---'-'         ";
-
+        // this looked better here https://www.asciiart.eu/cartoons/mickey-mouse
 }
